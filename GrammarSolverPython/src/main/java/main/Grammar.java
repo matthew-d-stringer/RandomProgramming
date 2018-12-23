2f51df00d1c9ac0d002f2ac24c5d3ccc37946659
Matthew Stringer <mdsflyboy@gmail.com>
2018-12-23 12:10:38 -0800

    first functioning version

--- GrammarSolverPython/src/main/java/main/Grammar.java
+++ GrammarSolverPython/src/main/java/main/Grammar.java
@@ -13,7 +13,10 @@ import java.util.regex.Pattern;
 public class Grammar{
     public static void main(String[] args) throws Exception {
         Map<String, ArrayList<String>> list = readFile("src/main/resources/grammar.txt");
-        System.out.println(formSentence(list));
+        ArrayList<String> output = formSentences(list,10);
+        for(String sent:output){
+            System.out.println(sent);
+        }
     }
 
     public static Map<String, ArrayList<String>> readFile(String file) throws Exception{
@@ -30,11 +33,13 @@ public class Grammar{
     }
 
     public static void readFileHelper(String line, Map<String, ArrayList<String>> list){
-        line.replaceAll("\\s+", "");
+        // line.replaceAll("\\s+", "");
         String[] splitLine = line.split(":");
         // System.out.println(splitLine[0]);
         // System.out.println(splitLine[1]);
         String var = splitLine[0];
+        var.replaceAll("\\s+", "");
+        // System.out.println(var);
         String[] out = splitLine[1].split("\\|");
         // System.out.println(out[0]);
         ArrayList<String> conditions = new ArrayList<>(Arrays.asList(out));
@@ -43,6 +48,9 @@ public class Grammar{
 
     public static ArrayList<String> formSentences(Map<String, ArrayList<String>> list, int num){
         ArrayList<String> out = new ArrayList<>();
+        for(int i = 0; i < num; i++){
+            out.add(formSentence(list));
+        }
         return out;
     }
 
@@ -54,8 +62,14 @@ public class Grammar{
             return key;
         else{
             ArrayList<String> possibilities = list.get(key);
-            int choice = (int)(Math.random()*(possibilities.size()-1));
-            return possibilities.get(choice);
+            int choice = (int)(Math.random()*(possibilities.size()));
+            String cString = possibilities.get(choice);
+            String[] parts = cString.split("\\s");
+            String out = "";
+            for(String part:parts){
+                out += formSentenceHelper(part, list)+" ";
+            }
+            return out;
         }
     }
 }
