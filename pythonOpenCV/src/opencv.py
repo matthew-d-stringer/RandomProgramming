f1a71ed86333c8ac8a96949208649b2e8e4c25ea
Matthew <mdsflyboy@gmail.com>
2019-03-13 14:58:06 -0700

    making the code more copy and pasteable

--- pythonOpenCV/src/opencv.py
+++ pythonOpenCV/src/opencv.py
@@ -5,18 +5,20 @@ from grip import GripPipeline
 W = 320
 H = 240
 
-img = cv2.imread('../resources/cargoship.png',cv2.IMREAD_COLOR)
-# img = cv2.imread('../resources/multipleTargets.png',cv2.IMREAD_COLOR)
+# img = cv2.imread('../resources/cargoship2.png',cv2.IMREAD_COLOR)
+img = cv2.imread('../resources/missingPart.png',cv2.IMREAD_COLOR)
 
 height, width, depth = img.shape
 
 scaleX = W/width
 scaleY = H/height
 newX,newY = img.shape[1]*scaleX, img.shape[0]*scaleY
-newimg = cv2.resize(img,(int(newX),int(newY)))
+# newimg = cv2.resize(img,(int(newX),int(newY)))
+newimg = cv2.resize(img,(int(320),int(240)))
 
 gripCode = GripPipeline()
-out = gripCode.process(img)
+out = gripCode.process(newimg)
+out = cv2.resize(out,(320*2,240*2))
 
 # cv2.imshow('img', newimg)
 cv2.imshow('out', out)
